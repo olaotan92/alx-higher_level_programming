43fe47821d0f3e96a6e2e26816a26adfdbd9b9ba
Olaotan Ojo <elijahshawntel@gmail.com>
2022-12-05 15:09:11 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char not in "cC":
-            new_string += char
-        return new_string
+    str = ""
+    for i in my_string[:]:
+        if i != 'c' and i != 'C':
+            str += i
+    return (str)
