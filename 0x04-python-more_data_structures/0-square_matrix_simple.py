21bf17f5d6c6f6c37aac36f616854a1840bb72b9
Olaotan Ojo <elijahshawntel@gmail.com>
2022-12-19 00:45:48 -0800

    more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    new_matrx = ([[c**2 for c in row] for row in matrix])
-    return(new_matrx)
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
